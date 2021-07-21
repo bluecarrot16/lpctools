01debc0d7749c664f5646a61b0f14fbd2d793e5f
bluecarrot16 <27840+bluecarrot16@users.noreply.github.com>
2021-07-21 11:29:07 -0400

    Setup python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='lpctools',
+      version='0.1',
+      description='Tools for pixel art in the Liberated Pixel Cup style',
+      url='http://github.com/bluecarrot16/lpctools',
+      author='bluecarrot16',
+      author_email='bluecarrot16@gmail.com',
+      license='MIT',
+      packages=['lpctools'],
+      scripts=['bin/lpctools'],
+      zip_safe=False)
